8a1623ecc3d60e01ee1f5b75695b656ed34ae4e3
vipin <mantrivipin17@gmail.com>
2019-03-17 16:17:41 +0530

    added recipe_edit component

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../../shared/services/recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,15 +10,19 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
-  
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, 
+    private router: Router) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.recipe = this.recipeService.getRecipe(+params['id']);
     });
   }
-
+  
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route } );
+  }
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   } 
